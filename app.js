160629100f5f3a4764855d4794184ad654b7aa8e
Desi Steib <desiwebdev11@gmail.com>
2022-07-17 12:00:15 -0400

    Added mobile features for links.

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-// const menuToggle = document.querySelector('.menuToggle');
-// const nav = document.querySelector('.header-nav');
-
-
+const header = document.querySelector('.header');
 let toggles = document.getElementsByClassName('footer-mobile__toggle');
 let mobileContent = document.getElementsByClassName('footer-mobile__content');
 let icons = document.getElementsByClassName('footer-mobile__icon');
 
-// Header Mobile Menu
 
 
-// function toggleMenu() {
-//   menuToggle.classList.toggle('active');
-//   nav.classList.toggle('active');
-// }
+
+// Sticky Header
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 150) {
+    header.classList.add('header--sticky');
+  } else {
+    header.classList.remove('header--sticky');
+  }
+});
+
+// Mobile Toggle
+document.onclick = () => {
+  let openMobile = document.getElementsByClassName('open__mobile-menu')[0];
+  let mobileMenu = document.getElementsByClassName('header-mobile')[0];
+  let closeMobile = document.getElementsByClassName('header-mobile__close-btn')[0];
+
+  
+  openMobile.addEventListener('click', () => {
+    mobileMenu.classList.add('header-mobile--visible');
+  });
+  closeMobile.addEventListener('click', () => {
+    mobileMenu.classList.remove('header-mobile--visible');
+  });
+}
+
 
 
 // Footer Mobile Menu
